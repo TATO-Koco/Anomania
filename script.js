b46a713265ebb6cceacbe85c0091f8671731dbe6
TATO <57866873+TATO-Koco@users.noreply.github.com>
2023-01-08 19:05:45 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,56 +1,130 @@
+// validation
 
-if (localStorage.getItem('login')) {
-    alert('Already Registered')
-    window.history.back()
+window.addEventListener('DOMContentLoaded', () => {
+const form = document.getElementById('Formi');
+const username = document.getElementById('username');
+const lastname = document.getElementById('lastname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const cpassword = document.getElementById('cpassword');
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
+
+    Validate();
+})
+
+const sendData = (usernameVal, sRate, Count) => {
+    if(sRate === Count){
+        swal("Hello " + usernameVal , "You are Registered", "success");
+    }
 }
 
-
-// //--------sign up--------
-let usernameInput = document.querySelector('#username')
-let emailInput = document.querySelector('#email')
-let passwordInput = document.querySelector('#password1')
-let _passwordInput = document.querySelector('#password2')
-let submitBtn = document.querySelector('#submitbtn')
-
-let values = {
-    username: "",
-    email: "",
-    password: "",
-    _password: "",
-}
-
-let getValues = () => {
-    values.username = usernameInput.value
-    values.email = emailInput.value
-    values.password = passwordInput.value
-    values._password = _passwordInput.value
-}
-
-
-let emailFilter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
-let passwordFilter = /^[A-Za-z]\w{7,20}$/
-let submitForm = () => {
-    getValues ()
-    
-    if (values.email.match(emailFilter) && values.password.match(passwordFilter) && values.password == values._password) {
-        localStorage.setItem('login', JSON.stringify(values))
-        window.history.back()
-    } else {
-        alert('Something incorrect')
+const SuccessMsg = (usernameVal) => {
+    let formContr = document.getElementsByClassName('form-control');
+    var Count = formContr.length - 1;
+    for(var i = 0; i < formContr.length; i++){
+        if(formContr[i].className === "form-control success"){
+            var sRate = 0 + i;
+            console.log(sRate);
+            sendData(usernameVal, sRate, Count);
+        }
+        else{
+            return false;
+        }
     }
 }
 
 
+const isEmail = (emailVal) =>{
+    var atSymbol = emailVal.indexOf('@');
+    if(atSymbol < 1) return false;
+    var dot = emailVal.lastIndexOf('.');
+    if(dot <= atSymbol + 2) return false;
+    if(dot === emailVal.length -1) return false;
+    return true;
+}
+
+function Validate(){
+    const usernameVal = username.value.trim();
+    const lastnameVal = lastname.value.trim();
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+    const cpasswordVal = cpassword.value.trim();
+
+    //username
+    if(usernameVal === ""){
+        setErrorMsg(username, 'first name cannot be blank');
+    }
+    else if(usernameVal.length <=2){
+        setErrorMsg(username, 'min 3 char');
+    }
+    else{
+        setSuccessMsg(username);
+    }
+
+    //last name
+
+    if(lastnameVal === ""){
+        setErrorMsg(lastname, 'last name cannot be blank');
+    }
+    else if(lastnameVal.length <=2){
+        setErrorMsg(lastname, 'min 3 char');
+    }
+    else{
+        setSuccessMsg(lastname);
+    }
+
+    //email
+    if(emailVal === ""){
+        setErrorMsg(email, 'email cannot be blank');
+    }
+    else if(!isEmail(emailVal)){
+        setErrorMsg(email, 'email is not valid');
+    }
+    else{
+        setSuccessMsg(email);
+    }
+
+    //password
+    if(passwordVal === ""){
+        setErrorMsg(password, 'password cannot be blank');
+    }
+    else if(passwordVal.length <= 7){
+        setErrorMsg(password, 'min 8 char');
+    }
+    else{
+        setSuccessMsg(password);
+    }
+
+    //confirm password
+    if(cpasswordVal === ""){
+        setErrorMsg(cpassword, 'confirm password cannot be blank');
+    }
+    else if(passwordVal != cpasswordVal){
+        setErrorMsg(cpassword, 'Not Matched!');
+    }
+    else{
+        setSuccessMsg(cpassword);
+    }
+    SuccessMsg(usernameVal);
 
 
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-const menu_items = document.querySelectorAll('nav .mainMenu li a');
+}
 
+function setErrorMsg(input, errormsgs){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = "form-control error";
+    small.innerText = errormsgs;
+}
 
+function setSuccessMsg(input){
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
 
-
+})
 
 
 // navbar
@@ -60,6 +134,7 @@ Hamburger.onclick = function() {
     navBar = document.querySelector(".navbar");
     navBar.classList.toggle("active");
 }
+
 
 
 // API
